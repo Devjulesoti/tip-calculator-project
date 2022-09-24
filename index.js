4960e317e6e31dcadd301a0ba8f03a5bf2e2b915
juliet oti <egwuotijulietchinenye@gmail.com>
2022-09-24 23:20:48 +0100

    commit

--- index.js
+++ index.js
@@ -8,43 +8,59 @@
 
     function calculate(){
         let result;
+        let totalAmount;
        let buttons = document.querySelectorAll('button');
 
        if (buttons[0].textContent === '5%') {
         result = Number(((5/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
        }
 
        else if (buttons[1].textContent === '10%'){
         result = Number(((10/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
+        
        }
 
        else if (buttons[2].textContent === '15%'){
         result = Number(((15/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
        }
 
        else if (buttons[3].textContent === '20%'){
         result = Number(((20/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
        }
 
        else if (buttons[4].textContent === '25%'){
         result = Number(((25/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
        }
        
        else if (buttons[5].textContent === '50%'){
         result = Number(((50/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
+
        }
 
        else if (button[6].textContent === 'Custom') {
-        let percentage = alert('Enter percentage');
-        alert(percentage);
+        let percentage = prompt('Enter percentage');
         result = Number(((percentage.value/100)*bill.value)* 1);
         tipAmount.textContent = `$${result}`;
+        totalAmount = (result * People.value);
+        totalPrice.textContent = `$${totalAmount}`;
        }
 
        else{
