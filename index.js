a13ccdef8417c2d28408a49af29ebb9be51a5006
juliet oti <egwuotijulietchinenye@gmail.com>
2022-10-05 10:14:44 +0100

    corrected

--- index.js
+++ index.js
@@ -1,84 +1,55 @@
 
     
     let bill = document.getElementById('bill');
-    let People = document.querySelector('.number');
+    let people = document.getElementById('people');
     let tipAmount = document.getElementById('tip-amount');
     let totalPrice = document.getElementById('total-price');
     let resetButton = document.getElementsByClassName('.reset-btn');
+    let buttons = document.querySelectorAll('.percent-btn');
+    let custom = document.getElementById('custom-btn');
     
+    
+    // let billInput = bill.value;
 
-    function calculate(){
-        let result;
-        let totalAmount;
-       let buttons = document.querySelectorAll('button');
+    // let peopleInput = people.value;
 
-       if (buttons[0].textContent === '5%') {
-        result = Number(((5/100)*bill.value)* 1);
-        tipAmount.textContent = `$${result}`;
-        totalAmount = (result * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-       }
+    buttons.forEach(function(value) {
+        value.addEventListener('click', function(){
+         let percent = parseInt(value.getAttribute('data-percent'))
+         let calculate = (percent/100) * bill.value
+         let total = calculate * people.value
+         tipAmount.innerHTML = calculate
+         totalPrice.innerHTML = total
+        })
+    })
 
-       else if (buttons[1].textContent === '10%'){
-        result = Number(((10/100)*bill.value)* 1);
-        tipAmount.textContent = `$${result}`;
-        totalAmount = (result * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-        
-       }
 
-       else if (buttons[2].textContent === '15%'){
-        result = Number(((15/100)*bill.value)* 1);
-        tipAmount.textContent = `$${result}`;
-        totalAmount = (result * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-       }
-
-       else if (buttons[3].textContent === '20%'){
-        result = Number(((20/100)*bill.value)* 1);
-        tipAmount.textContent = `$${result}`;
-        totalAmount = (result * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-       }
-
-       else if (buttons[4].textContent === '25%'){
-        result = Number(((25/100)*bill.value)* 1);
-        tipAmount.textContent = `$${result}`;
-        totalAmount = (result * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-       }
-       
-       else if (buttons[5].textContent === '50%'){
-        result = Number(((50/100)*bill.value)* 1);
-        tipAmount.textContent = `$${result}`;
-        totalAmount = (result * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-
-       }
-       else{
-        tipAmount.textContent = " ";
-       }
-
-    }
-
+    
     function resetCalc() {
         bill.value = '';
         bill.focus();
-        People.value = '';
+        people.value = '';
         tipAmount.textContent = '$0.00';
         totalPrice.textContent = '$0.00';
-
+        custom.placeholder = "Custom";
+        custom.value = 'Custom'
     }
 
-    function custom() {
-        let percentage;
-        let customResult;
-        let totalAmount;
-        customResult = prompt('Enter your desired percentage');
-        percentage = (Number(customResult.value)/100)*bill.value;
-        tipAmount.textContent = `$${percentage}`;
-        totalAmount = (percentage * People.value);
-        totalPrice.textContent = `$${totalAmount}`;
-    }
-    
+    function customCalc() {
+      let userInput = custom.value
+      console.log(userInput)
+        if (userInput > 0 && people.value > 0) {
+            let calculate = (userInput / 100) * bill.value
+            let total = calculate * people.value
+            tipAmount.innerHTML = calculate
+            totalPrice.innerHTML = total
+        }
 
+        else{
+        custom.value = ''
+        custom.placeholder = 'Custom'
+        }
+        
+
+        
+    }
